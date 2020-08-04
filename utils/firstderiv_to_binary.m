63d1c913f441df111d78b9b673d3d5a0bad42a10
ndominica <61783861+ndominica@users.noreply.github.com>
2020-08-04 17:27:46 +0200

    manualish spike detection method from carrillo-reid 2008

---
+++ utils/firstderiv_to_binary.m
@@ -0,0 +1,27 @@
+function [B] = firstderiv_to_binary(ex, istage)
+% [B] = firstderiv_to_binary(ex,istage) creates the binary matrix based
+% upon the preset threshold of 2.5 times the sd of noise from the first
+% derivative of dff calcium signals of each rois
+%
+% INPUTS:
+%       ex - experiment object
+%       istage - stage of measurement
+% OUTPUTS:
+%       B - binary matrix
+%
+%Part of ZENITH utils
+
+M=[];
+DXDT = [];
+for iroi = 1:ex.N_roi
+    m = ex.stitch(iroi, istage, 'dff');
+    dx = gradient(m.data);
+    dt = gradient(m.time);
+    dxdt = dx./dt;
+    M = [M;m];
+    DXDT = [DXDT; dxdt];
+end
+
+B = double(DXDT >= std2(DXDT)*2.5);
+
+
