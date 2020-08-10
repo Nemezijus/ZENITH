c02098785851e14c93205578524ba92e2c90e961
ndominica <61783861+ndominica@users.noreply.github.com>
2020-08-10 09:23:34 +0200

    rois that fire together...

--- utils/temporal_vectors_durstim.m
+++ utils/temporal_vectors_durstim.m
@@ -1,4 +1,4 @@
-function [TV, TVstim, STIMSAMP] = temporal_vectors_durstim(B, samples, STIMSAMP)
+function [unistim_cellpools, TV, TVstim, STIMSAMP] = temporal_vectors_durstim(B, samples, STIMSAMP, slist)
 % [TVstim] = temporal_vectors_durstim() - creates
 % temporal vector binary matrix where each column represents time point and
 % every row - 1 or 0 - whether the ROI is coactive or not.
@@ -9,26 +9,54 @@ function [TV, TVstim, STIMSAMP] = temporal_vectors_durstim(B, samples, STIMSAMP)
 %
 %
 %   OUTPUTS:
-%       TVstim - Temporal Vector matrix
+%       unistim_cellpools - struct
+%       TV - Temporal Vector matrix
+%       TVstim - Temporal Vector matrix, reduced to during stim only
+%       STIMSAMP - struct
 %
 %part of ZENITH
 
 ons = STIMSAMP.stim_on:STIMSAMP.full_s:STIMSAMP.full_xl;
 offs = STIMSAMP.stim_off:STIMSAMP.full_s:STIMSAMP.full_xl;
 
-samples_stim = []; samples_stimid = [];
+samples_stim = []; samples_stim_num = [];
 for m = 1:numel(samples)
     for n = 1:numel(ons)
         if samples(m) >= ons(n) && samples(m) <= offs(n)
             samples_stim = [samples_stim, samples(m)];
-            samples_stimid = [samples_stimid, n];
+            samples_stim_num = [samples_stim_num, n];
             break
         end 
     end
 end
 STIMSAMP.samps = samples_stim;
-STIMSAMP.samps_stimid = samples_stimid;
+STIMSAMP.samps_stimid = samples_stim_num;
 
 TV = zeros(size(B));
 TV(:,samples_stim) = B(:,samples_stim);
 TVstim = B(:,samples_stim);
+
+coact_cells = {};
+cells = 1:size(B,1);
+signif_vectors = 1:size(TVstim, 2);
+for ivec = 1:size(TVstim, 2)
+    cocell_mask = logical(TVstim(:,ivec));
+    coact_cells{1,ivec} = cells(cocell_mask);
+    coact_cells{2,ivec} = samples_stim_num(ivec);
+end
+
+dunistims = (diff([coact_cells{2,:}]) > 0);
+dunistims(end+1) = 0;
+stim_shift_idx = signif_vectors(logical(dunistims))+1;
+unistims = unique(samples_stim_num);
+unistim_cellpools(1).stim_num = unistims(1);
+unistim_cellpools(1).coactive_cells = unique([coact_cells{1,1:stim_shift_idx(1)-1}]);
+for n = 2:numel(stim_shift_idx)
+    unistim_cellpools(n).stim_num = unistims(n);
+    if n == numel(stim_shift_idx)
+        unistim_cellpools(n).coactive_cells = unique([coact_cells{1,stim_shift_idx(n):end}]);
+        break
+    end
+    unistim_cellpools(n).coactive_cells = unique([coact_cells{1,stim_shift_idx(n):stim_shift_idx(n+1)-1}]);
+end
+
