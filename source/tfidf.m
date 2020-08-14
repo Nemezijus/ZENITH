a801cfce9c8261643f206c637f58399f5fa814e3
ndominica <61783861+ndominica@users.noreply.github.com>
2020-08-14 10:51:14 +0200

    tfidf updated version

--- source/tfidf.m
+++ source/tfidf.m
@@ -1,28 +1,37 @@
 function [TFIDF] = tfidf(TVstim)
-% [tfid] = tfidf(TVstim)
-%
+% [tfid] = tfidf(TVstim) calculates tf*idf values for each roi within each
+% significant vectors during stimulus
 %
 %   INPUT(S):
-%       TVstim -
+%       TVstim - significant temporal vectors happening during the visual
+%       stimulation window
 %
 %   OUTPUT(S):
-%       TFIDF -
+%       TFIDF - matrix containing the tf-idf values
 %
-%Part of ZENITH
+%Part of ZENITH source
 
 %DEFINITIONS
 %TERM FREQUENCY (TF)
 %measures active neurons normalized by the total number of active neurons
 %for each frame
+% @(a,n)1/sum(a(:,n))
+% @(a,n)log10(1+sum(a(:,n)))
+% @(a,n)1+log10(sum(a(:,n)))
 %INVERSE DOCUMENT FREQUENCY(IDF)
 %measures the number of times that a neuron appears in the tital number of
 %significant vectors
+% @(a,n)log10(numel(a(n,:))/sum(a(n,:)))
+% @(a,n)log10(numel(a(n,:))/(1+sum(a(n,:))))+1
+% @(a,n)log10(1+numel(a(n,:))/sum(a(n,:)))
+% @(a,n)log10(numel(a(n,:))/sum(a(n,:)))+1
 
 TFIDF = zeros(size(TVstim)); % 1 x numel(significant_vectors)
 for isv = 1:size(TVstim,2)
-    tf = sum(TVstim(:,isv))/numel(TVstim(:,isv));
+%     tf = sum(TVstim(:,isv))/numel(TVstim(:,isv));
+    tf = 1/sum(TVstim(:,isv));
     for iroi = 1:size(TVstim,1)
-        idf = log(numel(TVstim(iroi,:))/sum(TVstim(iroi,:)));
+        idf = log10(numel(TVstim(iroi,:))/sum(TVstim(iroi,:))) + 1;
         if isinf(idf)
             idf = 0;
         end
