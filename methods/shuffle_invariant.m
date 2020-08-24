7d7a9ed67b6cda8536c2026e8821e08eb33ed8c5
Nemezijus <aplauska@femtonics.eu>
2020-08-24 15:40:24 +0200

    Update shuffle_invariant.m
    Shuffle invariant should be working fine now. Negative numbers in the matrix might cause issues

--- methods/shuffle_invariant.m
+++ methods/shuffle_invariant.m
@@ -9,23 +9,35 @@ function SH = shuffle_invariant(M)
 %       SH - shuffled matrix
 %
 %part of ZENITH
+
 sz = size(M);
-row_sums = sum(M,2);
-col_sums = sum(M,1);
+row_sums = sum(M~=0,2);
+col_sums = sum(M~=0,1);
 zero_rows = sum(M,2) == 0;
 SH = zeros(sz);
 for icol = sz(2):-1:1
-    toshuffle = M(~zero_rows,icol);
-    shuffled = toshuffle(randperm(numel(toshuffle)));
+    toshuffle = M(:,icol);
+    
+    %1 - collect non zeros and zeros
+    toplace = toshuffle(toshuffle~=0);
+    toplace = toplace(randperm(numel(toplace)));%when its not 1's and 0's
+    Z = toshuffle(toshuffle==0);
+    
+    %2 - certain placement
     still_needed = row_sums;
     certainly = still_needed==icol;
-    idx = find(shuffled, sum(certainly), 'first');
-    SH(certainly,icol) = shuffled(idx);
-    shuffled(idx) = [];
-    if sum(shuffled)~=4
-        a=1;
-    end
-    SH(~zero_rows&~certainly,icol) = shuffled;
-    row_sums = row_sums - SH(:,icol);
-    a=1;
+    certainly_idx = find(toplace, sum(certainly), 'first');
+    SH(certainly,icol) = toplace(certainly_idx);
+    toplace(certainly_idx) = [];
+    
+    %3 - place the remainder in random spots of the column
+    idx = find(still_needed<icol & still_needed~=0);
+    rand_idx = idx(randperm(numel(idx)));
+    SH(rand_idx(1:numel(toplace)),icol) = toplace;
+    SH(rand_idx(numel(toplace)+1:end),icol) = 0;
+    row_sums = row_sums - double(SH(:,icol)~=0);
+end
+
+if sum(SH~=0,1) ~= col_sums | sum(SH~=0,2) ~= row_sums
+    error('invariant shuffle did not succeed');
 end
