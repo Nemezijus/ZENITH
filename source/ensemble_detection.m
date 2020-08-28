1d141d02929b228671ad5049c15f70d8b0290e4d
Nemezijus <aplauska@femtonics.eu>
2020-08-28 18:53:42 +0200

    Update ensemble_detection.m
    added Ensemble output and computati

--- source/ensemble_detection.m
+++ source/ensemble_detection.m
@@ -1,4 +1,4 @@
-function [B,redB,s,TFIDF,STIMSAMP] = ensemble_detection(ex,M,istage)
+function [B,redB,s,TFIDF,STIMSAMP, ENS] = ensemble_detection(ex,M,istage)
 % ensemble_detection(ex,M,istage) - parent function that processes spiking
 % activity of experiment and estimates the ensembles within the population
 %
@@ -46,6 +46,7 @@ fprintf('\n');
 P = 5;
 % fprintf(['Performing ',num2str(PAR.Nshuffle),' shuffles across columns\n']);
 tic;
+PAR.Nshuffle = 100;
 [thr, SMAP_real, COMAP_real] = similaritythreshold(TFIDF, PAR.Nshuffle, P, 1);
 t = toc;
 fprintf(['STEP 4 - DONE. Running time: ', num2str(t), ' seconds\n']);
@@ -73,7 +74,26 @@ fprintf(['STEP 6 - DONE. Running time: ', num2str(t), ' seconds\n']);
 fprintf('SVD PROCESSED. B COMPONENTS CALCULATED (6) \n');
 fprintf('\n');
 
+fprintf('STEP 7 - DECODING ENSEMBLES\n');
+fprintf('\n');
+tic;
+[ENS] = ensemble_decoding(redB, TFIDF, STIMSAMP);
+t = toc;
 
+%plotting stimuli distribution for ensembles
+F = figure;
+set(F,'units', 'normalized', 'position', [0.0995 0.0954 0.81 0.75])
 
+N = 6;
+AX2 = autoaxes(F,2,ceil(N/2),[0.05 0.025 0.05 0.05],[0.025,0.05]);
+
+for in = 1:N
+    axes(AX2(in));
+    [h_counts, h_units] = hist(ENS(in).samps_stimid,unique(ENS(in).samps_stimid));
+    bar(h_units, h_counts);
+    title(['component ',num2str(in)]);
+end
+
+fprintf(['STEP 7 - DONE. Running time: ', num2str(t), ' seconds\n']);
 
 
